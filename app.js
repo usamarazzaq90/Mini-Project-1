872365c36863c5c9630de4496c401cafa2cfda59
usamarazzaq90 <usamarazzaq9014@gmail.com>
2025-02-03 11:51:51 +0500

    Completed part 1

--- app.js
+++ app.js
@@ -15,6 +15,15 @@ app.get('/',(req,res)=>{
     res.render("index")
 })
 
+app.get('/login',(req,res)=>{
+    res.render("login")
+})
+
+app.get('/profile',isLoggedIn,(req,res)=>{
+    console.log(req.user);
+    res.render("login")
+})
+
 app.post('/register',async(req,res)=>{
     let {email,password,username,name,age}=req.body;
     let user=await userModel.findOne({email});
@@ -35,6 +44,37 @@ app.post('/register',async(req,res)=>{
         })
     })
 })
+
+app.post('/login',async(req,res)=>{
+    let {email,password}=req.body;
+    let user=await userModel.findOne({email});
+    if(!user) return res.status(500).send("Something went wrong!")
+
+    bcryptjs.compare(password, user.password, function(err,result){
+        if(result){
+            let token=jwt.sign({email: email, id:user._id},"shh")
+            res.cookie("token",token)
+            res.status(200).send("You can login!")
+        }
+        else res.redirect('/login')
+    })
+})
+
+app.get('/logout',(req,res)=>{
+    res.cookie("token","")
+    res.redirect("/login")
+})
+
+//Protected Routes
+function isLoggedIn(req,res,next){
+    if(req.cookies.token==="") res.send("You must be logged in");
+    else{
+        let data=jwt.verify(req.cookies.token,"shh",)
+        req.user=data;
+        next();
+    }
+}
+
 
 // app.listen('3000')
 //or
